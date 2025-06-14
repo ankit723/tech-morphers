2d58ec0d4de68018f7d9f0dc6f000d4df8b9532f
Ankit Biswas <72332524+ankit723@users.noreply.github.com>
2025-06-14 11:15:35 +0530

    hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,6 @@ import "./globals.css";
 import { ThemeProvider } from "@/providers/themeProvider";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/next";
-import Hotjar from '@hotjar/browser';
-
-const siteId = 6434945;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
 
 const gothamBook = localFont({
   src: "../public/Gotham-font-family/Gotham/Gotham-Book.otf",
@@ -87,6 +81,24 @@ export default function RootLayout({
         {children}
         </ThemeProvider>
         <Analytics />
+        
+        {/* Hotjar Tracking Code */}
+        <Script
+          id="hotjar"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6434945,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </body>
     </html>
   );
